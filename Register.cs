c991d4b2caa5d52e176aa6f182c6f6245c48313e
Jash Bhavesh Shah <46564241+jashshah127@users.noreply.github.com>
2020-01-08 13:57:24 +0530

    Request File

---
+++ Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.Request
+{
+    public class Register
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Emailid { get; set; }
+        public string Hobbies { get; set; }
+    }
+}
---
+++ Response.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.Request
+{
+    public class Response
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Emailid { get; set; }
+        public string Hobbies { get; set; }
+    }
+}
